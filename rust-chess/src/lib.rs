ec7289ebf1403293c6d3b33b949b0ae52c0d06a5
Jan Reinhardt <Jan.Reinhardt@aitgmbh.de>
2020-03-27 22:44:56 +0100

    nearly done

--- rust-chess/src/lib.rs
+++ rust-chess/src/lib.rs
@@ -510,3 +510,172 @@ fn check_game_state(board: &BoardState, turn: Color) -> GameResult {
         GameResult::Pending
     }
 }
+
+
+//AI
+
+fn order_moves(moves: Vec<Move>, board: BoardState) -> Vec<Move> {
+    let mut moves_and_values: Vec<(Move, i32)> = moves.into_iter().map(|m| {
+        let target_value = calc_piece_value(board.fields[m.target as usize], m.target);
+        let source_value = calc_piece_value(board.fields[m.source as usize], m.source);
+        (m, target_value - source_value)
+    }).collect();
+
+    moves_and_values.sort_unstable_by(|(_, v1), (_, v2)| {
+         v1.cmp(v2)
+    });
+
+    return moves_and_values.into_iter().map(|(m, _v)| { m }).collect();
+}
+
+const PIECE_SQUARE_TABLE_PAWN: [i32; 64] = [ 
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+     5,  5, 10, 25, 25, 10,  5,  5,
+     0,  0,  0, 20, 20,  0,  0,  0,
+     5, -5,-10,  0,  0,-10, -5,  5,
+     5, 10, 10,-20,-20, 10, 10,  5,
+     0,  0,  0,  0,  0,  0,  0,  0
+];
+
+const PIECE_SQUARE_TABLE_KNIGHT: [i32; 64] = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50,
+];
+
+const PIECE_SQUARE_TABLE_BISHOP: [i32; 64] = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+];
+
+const PIECE_SQUARE_TABLE_ROOK: [i32; 64] = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+];
+
+const PIECE_SQUARE_TABLE_QUEEN: [i32; 64] = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+];
+
+const PIECE_SQUARE_TABLE_KING: [i32; 64] = [
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -30,-40,-40,-50,-50,-40,-40,-30,
+    -20,-30,-30,-40,-40,-30,-30,-20,
+    -10,-20,-20,-20,-20,-20,-20,-10,
+    20, 20,  0,  0,  0,  0, 20, 20,
+    20, 30, 10,  0,  0, 10, 30, 20
+];
+
+fn calc_piece_value(piece: Piece, position: u8) -> i32 {
+    if piece == Piece::Empty {
+        return 0;
+    }
+    let index = if is_same_color(piece, Color::Black) {
+        // mirror position around center
+        let old_x = position % 8;
+        let old_y = position / 8;
+
+        ((7 - old_y) * 8 + old_x) as usize
+    } else
+    {
+        position as usize
+    };
+
+    let kind = get_kind(piece);
+    match kind {
+        Kind::None => {
+            0
+        }
+        Kind::Pawn => {
+            100 + PIECE_SQUARE_TABLE_PAWN[index]
+        }
+        Kind::Bishop => {
+            300 + PIECE_SQUARE_TABLE_BISHOP[index]
+        }
+        Kind::Knight => {
+            300 + PIECE_SQUARE_TABLE_KNIGHT[index]
+        }
+        Kind::Rook => {
+            500 + PIECE_SQUARE_TABLE_ROOK[index]
+        }
+        Kind::Queen => {
+            900 + PIECE_SQUARE_TABLE_QUEEN[index]
+        }
+        Kind::King => {
+            0 + PIECE_SQUARE_TABLE_KING[index]
+        }
+    }
+}
+
+fn calc_board_value(board: BoardState) -> i32 {
+    board.fields.iter().enumerate().map(|(i, &p)| {
+        let sign = if is_same_color(p, Color::White) { 1 } else { -1 };
+        return sign * calc_piece_value(p, i as u8);
+    }).sum()
+}
+
+fn all_moves(boardState: &BoardState, color: Color) -> Vec<Move> {
+    let pieces = get_field_ids_of_pieces(color, boardState);
+
+    let all_moves = pieces.into_iter().flat_map(|field_id| {
+        let moves = calc_possible_moves(field_id, boardState);
+        wrap_moves(moves, field_id)
+    }).collect();
+
+    all_moves
+}
+
+fn wrap_moves(moves: Vec<u8>, field_id: u8) -> Vec<Move> {
+    moves.into_iter().map(|m| { Move {source: field_id.clone(), target: m} }).collect()
+}
+
+fn do_ai_move(source: u8, target: u8, board: &mut BoardState, turn: Color) -> GameResult {
+    move_piece(source, target, board);
+
+    let result = check_game_state(board, turn);
+
+    result
+}
+
+fn calculate_best_move(boardState: BoardState) -> (Move, u8) {
+    //let startTime = performance.now();
+    //moveCount = 0;
+
+    let selectedMove = calculate_best_half_move(Color::Black, boardState, 4, -99999, 99999);
+
+    //let endTime = performance.now();
+
+    // console.log("Selected move: " + get_coord_from_id(selectedMove[0].source) + " -> " + get_coord_from_id(selectedMove[0].target))
+    // console.log("Time: " + (endTime - startTime) + " ms");
+    // console.log("Move Count: " + moveCount);
+
+    return selectedMove;
+}
